05dca5f25149edc821b9fe6404db5a4842c24250
Miller <millershaw61@gmail.com>
2024-09-02 22:15:53 +0100

    Init
    The basic hello world output, and then taking in input via the CL. Start with the extreme basics and build

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main(){
+	hello_world()
+	take_cli_input()
+}
+
+func hello_world(){
+	fmt.Println("Hello, World")
+}
+
+func take_cli_input(){
+	var input string
+
+	fmt.Print("Enter your name: ")
+	_,err := fmt.Scanln(&input)
+	if err != nil {
+		fmt.Println("Error reading input:",err)
+		return
+	}
+	fmt.Print("Your name is ",input,", hi there ",input,"!\n")
+}
